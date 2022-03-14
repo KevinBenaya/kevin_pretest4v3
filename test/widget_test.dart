8055ec33adb943fa7143737ae4470dc906f3ec0e
KevinBenaya <darkfaust063@gmail.com>
2022-03-14 17:34:32 +0700

    preparation(kevin_pretest4v3)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kevin_pretest4v3/app.dart';
 
 import 'package:kevin_pretest4v3/main.dart';
 
